ce010e366a36d1f3b462c1b32cb2c7f687e1aa76
Nashtronaut <nashboisvert1@gmail.com>
2023-08-29 19:54:25 -0600

    fixed feedback on original script

--- scripts/bulk-parse.js
+++ scripts/bulk-parse.js
@@ -1,62 +1,67 @@
-const parser = require('./parse-json.js');
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
-
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+const parser = require("./parse-json");
 
 const DEFAULT_INPUT_DIR = `./source/PCT`;
 const DEFAULT_OUTPUT_DIR = `./json`;
 
 const bulkParse = (input, output) => {
-    if (!fs.existsSync(input) ) {
-        throw new Error(`${input} directory could not be found or opened.`);
-    }
+  const HELP_MESSAGE = `
+    Usage:
+        node ./scripts/bulk-parse.js [input]
+    `;
 
-    if (!fs.existsSync(output)) {
-        fs.mkdirSync(output);
-    } else {
-        fs.readdir(output, (err, files) => {
-            if (err) throw new Error(err);
+  const inputDir = process.argv[2];
+  if (!inputDir) {
+    console.log(HELP_MESSAGE);
+    process.exit(1);
+  }
 
-            files.forEach((file) => {
-                fs.unlink(path.join(output, file), (err) => {
-                    if (err) {
-                        throw new Error(err);
-                    } 
-                });
-            })
-        })
-    }
+  if (!fs.existsSync(input)) {
+    throw new Error(`${input} directory could not be found or opened.`);
+  }
 
-    fs.readdir(input, { withFileTypes: true }, (err, files) => {
-        if (err) throw new Error(err);
+  if (!fs.existsSync(output)) {
+    fs.mkdirSync(output);
+  } else {
+    fs.readdir(output, (error, files) => {
+      if (error) throw error;
 
-        let outputCount = 1;
+      files.forEach((file) => {
+        fs.unlink(path.join(output, file), (err) => {
+          if (err) {
+            throw new Error(err);
+          }
+        });
+      });
+    });
+  }
 
-        files.forEach((file) => {
-            let fileName = `json-pct-${outputCount}.json`;
-            let fileOutPath = path.join(output, fileName);
-            
-            parser(path.join(input, file.name)).then((result) => {
-                fs.writeFile(fileOutPath, result, (err) => {
-                    if (err) { 
-                        throw new Error(err);
-                    }
-                });
-            });
+  fs.readdir(input, { withFileTypes: true }, (err, files) => {
+    if (err) throw new Error(err);
 
-            outputCount++;
-        })
-    })
+    let outputCount = 1;
+
+    files.forEach((file) => {
+      let fileName = `json-pct-${outputCount}.json`;
+      let fileOutPath = path.join(output, fileName);
+
+      parser(path.join(input, file.name)).then((result) => {
+        fs.writeFile(fileOutPath, result, (err) => {
+          if (err) {
+            throw new Error(err);
+          }
+        });
+      });
+
+      outputCount++;
+    });
+  });
 };
 
 const main = async (input = DEFAULT_INPUT_DIR, output = DEFAULT_OUTPUT_DIR) => {
-    try {
-        bulkParse(input, output);
-    } catch (err) {
-        console.error("ERROR BULK PARSING... NOT OK")
-        console.error(err);
-    }
-}
+  bulkParse(input, output);
+};
 
-main(process.argv[2])
+main(process.argv[2]);
